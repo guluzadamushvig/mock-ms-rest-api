6bd5d865e9bde91f8ceaaf59fd4fd43e76cdd8b1
Mushvig <guluzadamushvig@gmail.com>
2022-10-08 06:39:50 +0400

    added RequestParam

--- src/main/java/az/rateit/backend/controller/UserController.java
+++ src/main/java/az/rateit/backend/controller/UserController.java
@@ -3,10 +3,7 @@ package az.rateit.backend.controller;
 import az.rateit.backend.model.UserResponse;
 import az.rateit.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +12,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    public UserResponse getUser(@PathVariable Long id){
+    public UserResponse getUser(@PathVariable Long id, @RequestParam String name){
         return userService.getUser(id);
     }
 }
